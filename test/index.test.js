fa34b7c95c9ba78265c5c8f17c972980c7bd7a63
timokie <xiaoshame@126.com>
2020-03-17 16:35:07 +0800

    添加测试用例

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,18 @@
 var expect = require('expect.js');
 const isCellphoneValid = require('../dist/index.cjs').isCellphoneValid
-
 // 手机号正则匹配test
 describe('test isCellphoneValid', function () {
     it('should expose a function', function () {
         expect(isCellphoneValid).to.be.a('function');
     });
-
-    it('should check phone numer', function () {
+    it('17621251706 shold be correct phone number', function () {
         expect(isCellphoneValid('17621251706')).to.be(true);
     });
-    it('should check phone numer', function () {
+    it('07621251706 should not be correct phone numer', function () {
         expect(isCellphoneValid('07621251706')).to.be(false);
+    });
+    it('0762125170 should not be correct phone numer', function () {
+        expect(isCellphoneValid('0762125170')).to.be(false);
     });
 });
 
